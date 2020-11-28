a4d6b2ea419ef7c76125040745543eb52e92ea14
tom <thomas@lelli-bouallou.com>
2020-11-29 00:03:49 +0100

    version du 28 novembre

--- generate_pdf.py
+++ generate_pdf.py
@@ -8,11 +8,12 @@
 ##
 
 import qrcode
-import datetime
+import datetime as dt
 import sys
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
+from datetime import datetime
 from PIL import ImageFont
 from PIL import ImageDraw
 from PIL import Image
@@ -59,32 +60,32 @@ def draw_first_page_layout():
 
 # Create crosses:
 def get_cross():
-    image = Image.new('RGB', (30, 30), color=(255, 255, 255))
+    image = Image.new('RGB', (20, 20), color=(255, 255, 255))
     image_draw = ImageDraw.Draw(image)
-    image_font = load_font(35)
-    image_draw.text((3, -4), f'x', (0, 0, 0), font=image_font)
+    image_font = load_font(20)
+    image_draw.text((2, -2), f'x', (0, 0, 0), font=image_font)
     return np.array(image)
 
 def check_motif_boxes(img_array, motifs):
     cross = get_cross()
     if "travail" in motifs:
-        img_array[523:553, 169:199] = cross
+        img_array[691:711, 114:134] = cross
     if "achats" in motifs:
-        img_array[617:647, 169:199] = cross
+        img_array[832:852, 114:134] = cross
     if "sante" in motifs:
-        img_array[732:762, 169:199] = cross
+        img_array[924:944, 114:134] = cross
     if "famille" in motifs:
-        img_array[817:847, 169:199] = cross
+        img_array[967:987, 114:134] = cross
     if "handicap" in motifs:
-        img_array[904:934, 169:199] = cross
+        img_array[1034:1054, 114:134] = cross
     if "balade" in motifs:
-        img_array[982:1012, 169:199] = cross
+        img_array[1078:1098, 114:134] = cross
     if "convocation" in motifs:
-        img_array[1114:1144, 169:199] = cross
+        img_array[1241:1261, 114:134] = cross
     if "missions" in motifs:
-        img_array[1195:1225, 169:199] = cross
+        img_array[1308:1328, 114:134] = cross
     if "enfants" in motifs:
-        img_array[1291:1321, 169:199] = cross
+        img_array[1352:1372, 114:134] = cross
     return img_array
 
 def draw_QR_code(img_array,first_name, last_name, birth_date, birth_city, address, leave_date, leave_hour, motifs):
@@ -93,7 +94,11 @@ def draw_QR_code(img_array,first_name, last_name, birth_date, birth_city, addres
 
     # Pas de diacritiques, est-ce normal ? (alors qu'il peut y en avoir dans les cases à remplir)
     # Oui, c'est comme c'est qu'est conçu le QR dans les fichiers sources du repo du gouv
-    qr_text = f"Cree le: {datetime.datetime.now().strftime('%d/%m/%Y a %H:%M')};\n" \
+    # qr_text = f"Cree le: {datetime.datetime.now().strftime('%d/%m/%Y a %H:%M')};\n" \
+
+    time_object = dt.datetime.strptime(leave_hour, '%H:%M') + dt.timedelta(minutes=-4)
+    
+    qr_text = f"Cree le: {leave_date} a {time_object.strftime('%H:%M')};\n" \
               f" Nom: {last_name};\n" \
               f" Prenom: {first_name};\n" \
               f" Naissance: {birth_date} a {birth_city};\n" \
@@ -110,22 +115,21 @@ def draw_QR_code(img_array,first_name, last_name, birth_date, birth_city, addres
 
 def fill_save_first_page(img_array, qr, first_name, last_name, birth_date, birth_city, current_city, address, leave_date, leave_hour):
     # Draw QR code
-    img_array[1344:1544, 910:1110] = np.array(qr)
+    img_array[1400:1600, 910:1110] = np.array(qr)
     
     img = Image.fromarray(img_array)
 
     draw = ImageDraw.Draw(img)
     font = load_font(22)
-    font_small = load_font(14)
 
-    draw.text((250, 282), f'{first_name} {last_name}', (0, 0, 0), font=font)
-    draw.text((250, 328), f'{birth_date}', (0, 0, 0), font=font)
-    draw.text((630, 328), f"{birth_city}", (0, 0, 0), font=font)
-    draw.text((280, 374), f"{address}", (0, 0, 0), font=font)
+    draw.text((250, 350), f'{first_name} {last_name}', (0, 0, 0), font=font)
+    draw.text((250, 410), f'{birth_date}', (0, 0, 0), font=font)
+    draw.text((500, 410), f"{birth_city}", (0, 0, 0), font=font)
+    draw.text((280, 470), f"{address}", (0, 0, 0), font=font)
 
-    draw.text((228, 1368), f"{current_city}", (0, 0, 0), font=font)
-    draw.text((205, 1416), f"{leave_date}", (0, 0, 0), font=font)
-    draw.text((535, 1416), f"{leave_hour}", (0, 0, 0), font=font)
+    draw.text((205, 1416), f"{current_city}", (0, 0, 0), font=font)
+    draw.text((205, 1476), f"{leave_date}", (0, 0, 0), font=font)
+    draw.text((535, 1476), f"{leave_hour}", (0, 0, 0), font=font)
 
     # No more in reconfinement
     # draw.text((948, 1443), datetime.datetime.now().strftime("%d/%m/%Y à %H:%M"), (0, 0, 0), font=font_small)
